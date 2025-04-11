b575393259e4560563d3e973afabc67f0f130d86
negans-soyez <131256734+negans-soyez@users.noreply.github.com>
2025-04-11 09:58:57 +0200

    j'ai changé avec le travail de martin la connexion  a la bdd dans mon bout de projet

--- src/main/java/dao/ConnexionBDD.java
+++ src/main/java/dao/ConnexionBDD.java
@@ -1,17 +1,22 @@
 package dao;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnexionBDD {
-    public static Connection getConnexion() throws SQLException, ClassNotFoundException {
+    public static Connection getConnexion() throws SQLException, ClassNotFoundException, IOException {
 
         /// A MODIF ? dossier config.properties ou changement de port global ?
-
-        String url = "jdbc:mysql://localhost:3308/attraction";
-        String user = "root";
-        String motDePasse = "";
+        Properties props = new Properties();
+        props.load(new FileInputStream("config.properties"));
+        String url = props.getProperty("db.url");
+        String user = props.getProperty("db.user");
+        String motDePasse = props.getProperty("db.password");
         Class.forName("com.mysql.cj.jdbc.Driver");
         return DriverManager.getConnection(url, user, motDePasse);
     }
