6de8d00fac59b0a4197b9d61c99734c515692060
ilir78 <132138974+ilir78@users.noreply.github.com>
2025-04-11 18:01:40 +0200

    Create ReservationDAO.java
    code dispo qui permettra quand le bouton reserver sera dispo de faire une reservation avc num client, num attraction et la date dans la bdd.

---
+++ src/main/java/Modele/ReservationDAO.java
@@ -0,0 +1,67 @@
+package Modele;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReservationDAO {
+
+    private Connection conn;
+
+    public ReservationDAO(Connection conn) {
+        this.conn = conn;
+    }
+    public void ajouterReservation(int idClient, int idAttraction, Date dateAttraction) throws SQLException {
+        String sql = "INSERT INTO reservation (idClient, idAttraction, dateAttraction, dateReservation) VALUES (?, ?, ?, CURDATE())";
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setInt(1, idClient);
+            stmt.setInt(2, idAttraction);
+            stmt.setDate(3, dateAttraction);
+            stmt.executeUpdate();
+        }
+    }
+
+
+    public void supprimerReservation(int idReservation) throws SQLException {
+        String sql = "DELETE FROM reservation WHERE idReservation = ?";
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setInt(1, idReservation);
+            stmt.executeUpdate();
+        }
+    }
+
+
+    public void modifierDateReservation(int idReservation, Date nouvelleDate) throws SQLException {
+        String sql = "UPDATE reservation SET dateAttraction = ? WHERE idReservation = ?";
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setDate(1, nouvelleDate);
+            stmt.setInt(2, idReservation);
+            stmt.executeUpdate();
+        }
+    }
+
+
+    public List<String> getReservationsParClient(int idClient) throws SQLException {
+        List<String> reservations = new ArrayList<>();
+        String sql = "SELECT r.idReservation, a.nom, r.dateAttraction " +
+                "FROM reservation r " +
+                "JOIN attraction a ON r.idAttraction = a.idAttraction " +
+                "WHERE r.idClient = ?";
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setInt(1, idClient);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                String res = "Réservation #" + rs.getInt("idReservation") +
+                        " - Attraction : " + rs.getString("nom") +
+                        " - Date : " + rs.getDate("dateAttraction");
+                reservations.add(res);
+            }
+        }
+        return reservations;
+
+
+        /// A AJOUTER DANS LE CONTROLEUR GRAPHIQUE (bouton pr reserver) :
+        ///ReservationDAO dao = new ReservationDAO(ConnexionBDD.getConnection());
+        ///dao.ajouterReservation(idClient, idAttraction, dateChoisie);
+    }
+}
