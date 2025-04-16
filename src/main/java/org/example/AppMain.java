4566eb63d1469300d53f7c2d98eca241601d32ff
PacomeECE <104898470+PacomeECE@users.noreply.github.com>
2025-04-16 10:08:57 +0200

    rename vueConnexion

--- src/main/java/org/example/AppMain.java
+++ src/main/java/org/example/AppMain.java
@@ -1,16 +1,13 @@
 package org.example;
 
-import vue.ConnexionApp;
+import vue.vueConnexion;
 import javafx.application.Application;
-import vue.VueCalendrier;
-
-import javax.swing.*;
 
 public class AppMain {
     public static void main(String[] args) {
 
 
-        Application.launch(ConnexionApp.class, args);  // Lancement JavaFX propre
+        Application.launch(vueConnexion.class, args);  // Lancement JavaFX propre
 
     }
 }
