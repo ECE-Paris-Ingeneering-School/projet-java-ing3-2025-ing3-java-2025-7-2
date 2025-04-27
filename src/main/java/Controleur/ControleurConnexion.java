f93710875e198479fc030d57956f35b09c440663
ilir78 <132138974+ilir78@users.noreply.github.com>
2025-04-27 22:27:06 +0200

    Update ControleurConnexion.java
    javadoc

--- src/main/java/Controleur/ControleurConnexion.java
+++ src/main/java/Controleur/ControleurConnexion.java
@@ -6,13 +6,23 @@ import modele.AuthentificationService;
 
 import java.sql.Connection;
 
+/**
+ * ControleurConnexion gère la logique de connexion des utilisateurs.
+ */
 public class ControleurConnexion {
-    public Utilisateur connecter(String email, String mdp){
+
+    /**
+     * Tente de connecter un utilisateur en vérifiant ses identifiants.
+     *
+     * @param email Adresse email saisie
+     * @param mdp Mot de passe saisi
+     * @return Utilisateur connecté si succès, sinon null
+     */
+    public Utilisateur connecter(String email, String mdp) {
         try (Connection conn = ConnexionBDD.getConnexion()) {
             AuthentificationService auth = new AuthentificationService();
             return auth.connecter(email, mdp, conn);
-
-        }catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
